8eb88c5e415cf9e242bb9142e07ab5c37d68ea5a
Christian Chourio <christianchouriox@hotmail.com>
2026-08-01 14:31:19 -0400

    Add onChange and onClick

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,12 @@ import './TodoSearch.css'
 
 function TodoSearch() {
     return(
-        <input className='TodoSearch' placeholder='Search' />
+        <input className='TodoSearch' placeholder='Search' onChange={(event) => {
+            console.log('Search')
+            console.log(event)
+            console.log(event.target)
+            console.log(event.target.value)
+        }} />
     );
 }
 
